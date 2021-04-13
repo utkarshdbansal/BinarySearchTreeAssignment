edfb5d06c061146efce86453aa21dbdc480e82e9
Utkarsh <utkarshdbansal@gmail.com>
2021-04-13 23:25:47 +0530

    node class committed

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -1,5 +1,36 @@
 package implementation;
 
 public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
-    //complete node class
+    E data;
+    Node<E> right;
+    Node<E> left;
+    public Node(E data){
+        this.data=data;
+        this.right=null;
+        this.left=null;
+
+    }
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+
+    public Node<E> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
 }
