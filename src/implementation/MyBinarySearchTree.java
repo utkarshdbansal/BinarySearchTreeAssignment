c4913a275f1b19a6c73e97243430bd560301b00e
Utkarsh <utkarshdbansal@gmail.com>
2021-04-13 23:33:30 +0530

    preOrder method completed

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -63,7 +63,11 @@ public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearch
 
     @Override
     public void preOrder(implementation.Node<E> node) {
-
+        if(node != null) {
+            System.out.print(node.getData() + ", ");
+        }
+        preOrder(node.getLeft());
+        preOrder(node.getRight());
     }
 
     @Override
